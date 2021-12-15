b6c10b07b9d697b48b7b7cf1f3588c147a8239b5
SerjoWeb <sergeyweb87@gmail.com>
2021-12-15 17:30:49 +0800

    Update library

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# custom_js_library_practice
-small js library like jQuery, just practice
+# Custom JS Library
+## SerjoJS
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+A custom JS Library similar to JQuery with only minimum functionality.
+Created and still under development.
+Main purpose to understand how JS works and how to create and develop your own libraries.
+
+- Pure JavaScript
+- ECMA SCRIPT latest
+
+Still under testing, development
+
+## How to use
+
+- Download SerjoJS.beta.js
+- Include to your project in the end of the body tag
+- Initialize SerjoJS Library
+
+```sh
+ const $js = new SerjoJS();
+```
+
+## Available Functions
+
+| Function | Description |
+| ------ | ------ |
+| ``` get() ``` | Function for select dom element |
+| ``` html() ``` | Function for setting up text and dom elements into selected dom element |
+| ``` append() ``` & ``` prepend() ``` | Functions for add dom elements before and after into selected dom element |
+| ``` css() ``` | Function for setting up styles |
+| ``` timeout() ``` | Function for setting up timeouts |
+| ``` ajax() ``` | Function for http/https requests |
+
+## Demo code
+
+```sh
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test js library</title>
+</head>
+<body>
+    <div class="test">
+        <span>Text 1</span>
+        <p>Text 2</p>
+    </div>
+
+    <div id="block">
+        <p>Text text</p>
+    </div>
+
+    <div id="block2"></div>
+    
+    <script src="SerjoJS.beta.js"></script>
+    <script>
+        const $js = new SerjoJS();
+
+        $js.get('.test span').html('Text 3');
+
+        $js.get('.test p').css({
+            color: '#fff',
+            backgroundColor: '#000',
+            display: 'flex',
+            width: 'fit-content'
+        });
+
+        const val1 = 150;
+        const val2 = 500;
+
+        $js.get('#block').append('div', {class: 'test-1', id: 'tes-id-1'}, `value: ${val1}`);
+
+        $js.get('#block').prepend('div', {class: 'test-2', id: 'tes-id-2'}, `value: ${val2}`);
+
+        $js.timeout(() => { console.log('test'); }, 100);
+
+        $js.ajax({
+            method: 'GET',
+            path: 'index.html',
+            headers: {
+                'X-Auth': 'auth-key',
+                'Content-type': 'application/x-www-form-urlencoded'
+            },
+            params: {
+                name: 'Mike',
+                age: 23,
+                position: false,
+                contract: 'yes'
+            }
+        }, (response) => {
+            console.log(response);
+        });
+    </script>
+</body>
+</html>
+```
---
+++ SerjoJS.beta.js
@@ -0,0 +1,220 @@
+/**
+ * JavaScript small library.
+ * Similiar to JQuery or same as.
+ * Practicing and deep learning about JS and new features.
+ * Author: SerjoWeb (sergeyweb87@gmail.com).
+ * Free License. Open Code.
+ */
+
+class SerjoJS {
+    constructor() {
+        
+    }
+
+    get(selector) {
+        try {
+            if (typeof selector === 'string') {
+                this.el = document.querySelectorAll(selector)[0];
+            } else {
+                console.error('invalid element');
+            }
+        
+            return this;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    /**
+     * html()
+     * function similiar to JQuery - html
+     */
+    html(val) {
+        try {
+            this.el.innerHTML = val;
+            return this;
+        } catch (err) {
+            console.err(err);
+        }
+    }
+
+    /**
+     * append()
+     * function similiar to JQuery - append
+     */
+    append() {
+        try {
+            if (arguments.length > 0) {
+                if (
+                    arguments.length === 3 && 
+                    typeof arguments[0] === 'string' && 
+                    typeof arguments[1] === 'object' && 
+                    typeof arguments[2] === 'string'
+                ) {
+                    let create = document.createElement(arguments[0]);
+                        create.innerHTML = arguments[2];
+    
+                    if (arguments[1]) {
+                        for (let attr in arguments[1]) {
+                            create.setAttribute(attr, arguments[1][attr]);
+                        }
+                    }
+                  
+                    this.el.appendChild(create);
+                } else {
+                    console.error('invalid arguments');
+                }
+            } else {
+                console.error('invalid arguments length');
+            }
+
+            return this;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    /**
+     * prepend()
+     * function similiar to JQuery - prepend
+     */
+     prepend() {
+        try {
+            if (arguments.length > 0) {
+                if (
+                    arguments.length === 3 && 
+                    typeof arguments[0] === 'string' && 
+                    typeof arguments[1] === 'object' && 
+                    typeof arguments[2] === 'string'
+                ) {
+                    let create = document.createElement(arguments[0]);
+                        create.innerHTML = arguments[2];
+    
+                    if (arguments[1]) {
+                        for (let attr in arguments[1]) {
+                            create.setAttribute(attr, arguments[1][attr]);
+                        }
+                    }
+            
+                    this.el.insertBefore(create, this.el.firstChild);
+                } else {
+                    console.error('invalid arguments length');
+                }
+            } else {
+                console.error('invalid arguments length');
+            }
+    
+            return this;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    /**
+     * css()
+     * function similiar to JQuery - css
+     */
+    css() {
+        try {
+            if (arguments.length > 0) {
+                if (arguments.length === 1 && typeof arguments[0] === 'object') {
+                    for (let arg in arguments[0]) {
+                        this.el.style[arg] = arguments[0][arg];
+                    }
+                } else {
+                    console.error('invalid arguments');
+                }
+            } else {
+                console.error('invalid arguments length');
+            }
+      
+            return this;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    /**
+     * ajax()
+     * function similiar to JQuery - ajax
+     */
+    ajax(data, callback) {
+        try {
+            if (data && callback) {
+                if (typeof data === 'object') {
+                    let xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = (e) => {
+                            if (e.target.readyState === 4 && e.target.status === 200) {
+                                if (typeof callback === 'function') {
+                                    callback(e.target.response);  
+                                }
+                            }
+                        };
+    
+                    xhttp.open(data.method, data.path, true);
+
+                    if (data.headers) {
+                        if (typeof data.headers === 'object') {
+                            for (let header in data.headers) {
+                                xhttp.setRequestHeader(header, data.headers[header]);
+                            }
+                        }
+                    }
+
+                    if (data.params) {
+                        if (typeof data.params === 'object') {
+                            let send = '';
+                            let i = 0;
+
+                            for (let param in data.params) {
+                                i++;
+
+                                if (i > 1) {
+                                    send += '&'+param +'='+data.params[param];
+                                } else {
+                                    send += param +'='+data.params[param];
+                                }
+                            }
+
+                            xhttp.send(send);
+                        } else {
+                            console.error('invalid arguments');
+                        }
+                    } else {
+                        xhttp.send();
+                    }
+                }
+            } else {
+                console.error('invalid arguments length');
+            }
+    
+            return this;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    /**
+     * timeout()
+     * function similiar to JQuery - timeout
+     */
+    timeout(callback, time) {
+        try {
+            if (callback && time) {
+                if (typeof time === 'number' && typeof callback === 'function') {
+                    setTimeout(() => {
+                        callback();
+                    }, time);
+                } else {
+                    console.error('invalid arguments');
+                }
+            } else {
+                console.error('invalid arguments length');
+            }
+
+            return this;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
